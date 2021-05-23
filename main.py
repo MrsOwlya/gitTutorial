02b46fc2746d3705d5f4e76d65ae1e3fe8e9ef61
MrsOwlya <olja-holmes@mail.ru>
2021-05-23 16:50:28 +0300

    Created telegram weather bot

--- main.py
+++ main.py
@@ -2,63 +2,44 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import math
-from colorama import Fore, Back, Style
 import pyowm
 import telebot
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-def weather():
-    owm = pyowm.OWM('1690d76ad190b1a89bf446288d3ad850')
-    place = input("Input your location: ")
-    mgr = owm.weather_manager()
-    observation = mgr.weather_at_place(place)
-    w = observation.weather()
-    print(w)
-
-def calculator():
-    what = input("What to do (+, -, *, /): ")
-    a = input("Input first number: ")
-    b = input("Input second number: ")
-    if what == "+":
-        otvet = int(a) + int(b)
-        print("Answer is: " + str(otvet))
-    elif what == "+":
-        otvet = int(a) - int(b)
-        print("Answer is: " + str(otvet))
-    elif what == "+":
-        otvet = int(a) * int(b)
-        print("Answer is: " + str(otvet))
-    elif what == "+":
-        otvet = int(a) / int(b)
-        print("Answer is: " + str(otvet))
-    else: print("Incorrect symbol!")
+from pyowm.utils.config import get_default_config
 
 
 bot = telebot.TeleBot("1802555647:AAGV-mNEgc92GxLT2Dlgd2C0PM9MctCGlI8")
+config_dict = get_default_config()
+config_dict['language'] = 'ru'
+owm = pyowm.OWM('1690d76ad190b1a89bf446288d3ad850', config_dict)
 @bot.message_handler(content_types=["text"])
 
 def telegrambot(message):
-    bot.reply_to(message, message.text)
+    if message.text == "/start" or message.text == "/help":
+        bot.send_message(message.chat.id, "Привет! Напиши мне название города на английском и я расскажу тебе о погоде")
+    else:
+        mgr = owm.weather_manager()
+        try:
+            observation = mgr.weather_at_place(message.text)
+        except:
+            bot.send_message(message.chat.id, "Извините, я не знаю такого города :(\nПопробуйте еще раз")
+            return
+        w = observation.weather
+        reg = owm.city_id_registry()
+        loc = reg.locations_for(message.text)
+        town = loc[0]
+        one_call = mgr.one_call(town.lat, town.lon)
+        forecast = one_call.forecast_daily
+        temp = w.temperature('celsius')['temp']
+        answer = "В городе "+message.text+" сейчас "+w.detailed_status+"\n"
+        answer += "Температура в районе "+str(temp)+"\n\n"
+        answer += "Завтра в это же время ожидается "+forecast[0].detailed_status+"\n"
+        answer += "Температура утром "+str(forecast[0].temperature('celsius')['morn']) \
+                  +", днем "+str(forecast[0].temperature('celsius')['day']) \
+                  +", вечером "+str(forecast[0].temperature('celsius')['eve']) \
+                  +", ночью "+str(forecast[0].temperature('celsius')['night'])
+
+        bot.send_message(message.chat.id, answer)
 
 bot.polling(none_stop=True)
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    #print("I \"love\" onion") # Экранирование
-    #print("Hello \nworld") # перенос строки
-    #myname = "Owlya"
-    #age = 23
-    #print("Hi, my name is " + myname + "!")
-    #print("I am " + str(age) + " years old") # Convert to string
-    #username = input("Input your name: ")
-    #userage = input("Input your age: ")
-    #print("Hi, I am " + username + ", i am " + userage + " years old") # Все, что вводит пользователь - строка!
-    #calculator()
-    #weather()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
